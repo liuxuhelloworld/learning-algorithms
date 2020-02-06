36ab4b2446782f4f2a85417f69abd546edead40f
liuxuhelloworld <liuxuhelloworld@126.com>
2020-02-06 10:08:21 +0800

    selection sort

---
+++ src/algs4/sort/Selection.java
@@ -0,0 +1,25 @@
+package algs4.sort;
+
+import edu.princeton.cs.algs4.In;
+
+public class Selection {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (Util.less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            Util.exch(a, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] a = new In().readAllStrings();
+        sort(a);
+        assert Util.isSorted(a);
+        Util.show(a);
+    }
+}
