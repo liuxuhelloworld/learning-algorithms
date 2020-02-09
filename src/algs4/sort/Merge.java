63148550e7b90fabce30ae2db3e667bde4320237
liuxuhelloworld <liuxuhelloworld@126.com>
2020-02-09 17:02:35 +0800

    merge sort, bottom-up version

--- src/algs4/sort/Merge.java
+++ src/algs4/sort/Merge.java
@@ -2,6 +2,10 @@ package algs4.sort;
 
 import edu.princeton.cs.algs4.In;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Merge {
     private static Comparable[] copy;
 
@@ -40,10 +44,29 @@ public class Merge {
         merge(a, lo, mid, hi);
     }
 
+    public static void bottomUpSort(Comparable[] a) {
+        int N = a.length;
+
+        copy = new Comparable[N];
+
+        for (int sz = 1; sz < N; sz = sz*2) {
+            for (int i = 0; i < N-sz; i += sz*2) {
+                merge(a, i, i+sz-1, Math.min(i+sz-1+sz, N-1));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String[] a = new In().readAllStrings();
         sort(a);
         assert Util.isSorted(a);
         Util.show(a);
+
+        List<String> alist = Arrays.asList(a);
+        Collections.shuffle(alist);
+        a = alist.toArray(new String[0]);
+        Util.show(a);
+        bottomUpSort(a);
+        assert Util.isSorted(a);
     }
  }
